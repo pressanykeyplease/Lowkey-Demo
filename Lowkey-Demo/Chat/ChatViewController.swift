59b0f5983490e0c10cb7bd6c0d374a89faa7c760
Eduard <51046544+pressanykeyplease@users.noreply.github.com>
2023-04-23 13:06:34 +0300

    Mock texts updated

--- Lowkey-Demo/Chat/ChatViewController.swift
+++ Lowkey-Demo/Chat/ChatViewController.swift
@@ -51,10 +51,11 @@ class ChatViewController: UIViewController {
     private let toolbar = ChatToolbar()
 
     private var messages: [MessageInfo] = [
+        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "I’m in also! Mike’s Diner would be a good choice 🔥🔥🔥 how about everyone else? Any ideas?")),
+        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Sounds good to me!!!")),
+        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "@kellyhodges are you in???")),
         .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Nice! 12 ppl in total. Let’s gather at the metro station!🚆🚆🚆")),
-        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Nice! 12 ppl in total. Let’s gather at the metro station!🚆🚆🚆")),
-        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Nice! 12 ppl in total. Let’s gather at the metro station!🚆🚆🚆")),
-        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Nice! 12 ppl in total. Let’s gather at the metro station!🚆🚆🚆")),
+        .text(TextMessageInfo(userpic: UIImage(named: "elon-musk"), name: "Elon Musk", message: "Okie dokie!!")),
         .poll(PollInfo(userpic: UIImage(named: "elon-musk"), pollType: "Public Poll", username: "Elon Musk", message: "What is the greatest NBA team in the history?", numberOfVotes: 3, options: ["Los Angeles Lakers", "Golden State Warriors", "Chicago Bulls", "Boston Celtics"], selectedOption: nil))
     ]
 }
